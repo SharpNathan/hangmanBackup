6b36cdcf727a61d80757d689db42dab61b2112a1
SharpNathan <56047117+SharpNathan@users.noreply.github.com>
2019-10-23 13:25:28 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,13 +1,13 @@
 import random
 while True:
-	myWord = input("Type the word, type random for a random word: ")
-	randomWord = ["jazz", "jail", "scratch", "smell", "apple", "water", "honey", "river", "random", "happy", "smile", "cheer", "avacado", "orange", "word", "desert", "dazzle", "mirage", "saloon", "dust", "hill", "sand", "metropolis", "misisippi", "oil", "ocean", "emerald", "green", "chemical", "plant", "marble", "spring", "yard", "labrynth", "star", "light", "scrap", "brain", "aquatic", "ruin", "casino", "night", "top", "mystic", "cave", "sky", "chase", "wing", "fortress", "death", "egg", "palm", "tree", "panic", "colision", "chaos", "tidal", "tempest", "quartz", "quadrant", "wacky", "work", "bench", "workbench", "stardust", "dust", "speedway", "speed", "way", "metalic", "madness", "metal", "mad", "angel", "island"]
-	if myWord == "random":
+	myWord = input("Type the word, leave blank for a random word: ")
+	randomWord = ["jazz", "jail", "scratch", "smell", "apple", "water", "honey", "river", "random", "happy", "smile", "cheer", "avacado", "orange", "word", "desert", "dazzle", "mirage", "saloon", "dust", "hill", "sand", "metropolis", "misisippi", "oil", "ocean", "emerald", "green", "chemical", "plant", "marble", "spring", "yard", "labrynth", "star", "light", "scrap", "brain", "aquatic", "ruin", "casino", "night", "top", "mystic", "cave", "sky", "chase", "wing", "fortress", "death", "egg", "palm", "tree", "panic", "colision", "chaos", "tidal", "tempest", "quartz", "quadrant", "wacky", "work", "bench", "workbench", "stardust", "dust", "speedway", "speed", "way", "metalic", "madness", "metal", "mad", "angel", "island", "hydro", "city", "garden", "carnival", "ice", "cap", "launch", "base", "mushroom", "valley", "flying", "fly", "battery", "lava", "reef", "hidden", "palace", "sanctuary", "doomsday", "doom", "day", "highway", "escape", "seaside", "sea", "side", "crisis", "rooftop", "roof", "run", "planet", "wisp", "studio", "super", "press", "titanic", "monarch", "reverie", "hyper", "ultimate", "technology", "the", "or", "and", "if", "not", "sorry", "i", "cant", "repeat", "but", "burn", "to", "ash", "please", "thank", "you", "diamond", "ruby", "saphire", "gem", "stone", "jewel"]
+	if myWord == "":
 		myWord = random.choice(randomWord)
 	else:
 		pass
 	score = 0
-	attempts = input("How many attempts do you want before guessing? ")
+	attempts = input("How many attempts do you want before guessing? (I recommend 8) ")
 	for x in range(int(attempts)):
 		letter = input("Guess a letter: ")
 		if letter in myWord:
@@ -26,3 +26,10 @@ while True:
 	else:
 		print("sorry no good.")
 		print("The word was: " + myWord)
+	playAgain = input("Do you want to play again? y/n ")
+	if playAgain == "y":
+		pass
+	elif playAgain == "n":
+		break
+	else:
+		print("Error: " + playAgain + " was not a choice")
